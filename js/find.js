bdad1bc5039a46d2df9db297d9260a7f70fc8b32
Tosya2000 <58148091+Tosya2000@users.noreply.github.com>
2021-07-06 12:19:04 +0300

    V50_06/07

--- js/find.js
+++ js/find.js
@@ -1,22 +1,41 @@
 document.querySelector('#id13003, #id20000 , #id30000, #id40000, #id50000').oninput=function(){
 	let val = this.value.trim();
 	let elasticItems = document.querySelectorAll('.accordion-header p, .sec3_mes_1_dop p, .table-responsive td');
+	let elastItems=document.querySelectorAll('.accordion-collapse, .accordion-button');
+	let elastItems1=document.querySelectorAll('.accordion-button');
 	if ( val!= ''){
+		//document.querySelector('#collapseOne').classList.add('show');
+		//document.querySelector('#collapseOne_1').classList.add('show');
+		//document.querySelector('#collapseOne_2').classList.add('show');
+		//document.querySelector('#collapseTwo').classList.add('show');
+		//document.querySelector('.accordion-button').classList.remove('collapsed');
+		elastItems.forEach(function(eln){
+				eln.classList.add('show');
+		});
+		elastItems1.forEach(function(eln){
+				eln.classList.remove('collapsed');
+		});
+		
 		elasticItems.forEach(function(elem){
 			if(elem.innerText.search(new RegExp(val, "i"))==-1){
-				elem.classList.add('hide');
+				//elem.classList.add('hide');
 				elem.innerHTML=elem.innerText;
 			}
 			else{
-				elem.classList.remove('hide');
+				//elem.classList.remove('hide');
 				let str=elem.innerText;
 				elem.innerHTML=insertMark(str,elem.innerText.search(new RegExp(val, "i")),val.length);
 			}
 		});
 	}
 	else {
+			//document.querySelector('.accordion-button').classList.add('collapsed');
+			//document.querySelector('.accordion-collapse').classList.remove('show');
+			//document.querySelector('#collapseOne_1').classList.remove('show');
+			//document.querySelector('#collapseOne_2').classList.remove('show');
+			//document.querySelector('#collapseTwo').classList.remove('show');
 		elasticItems.forEach(function (elem){
-			elem.classList.remove('hide');
+			//elem.classList.remove('hide');
 			elem.innerHTML=elem.innerText;
 		});
 	}
